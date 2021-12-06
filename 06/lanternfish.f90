9e78161dca2d1fa12cec4af28ce5067384081bb0
Corey Porter <coreyaporter@gmail.com>
2021-12-06 12:59:08 -0800

    part two

--- 06/lanternfish.f90
+++ 06/lanternfish.f90
@@ -1,36 +1,35 @@
 program Lanternfish
-   implicit none
-   integer, parameter :: MAX = 10000000
-   integer, parameter :: INPUT_SIZE = 300
-   integer, dimension(MAX) :: fish
-   integer :: i, to_add, total, n_fish, rounds
+    implicit none
+    integer, parameter :: MAX = 8
+    integer, parameter :: INPUT_SIZE = 300
+    integer(kind = 8), dimension(0:MAX) :: fish
+    integer(kind = 8) :: i, tmp, to_add, total, n_fish, rounds
 
-   do i = 1, MAX
-      fish(i) = 0
-   end do
+    do i = 0, MAX
+        fish(i) = 0
+    end do
 
-   open(1, file = 'input-fortran.txt', status = 'old')
-   do i = 1, INPUT_SIZE
-      read(1, *) fish(i)
-   end do
-   close(1)
+    open(1, file = 'input-fortran.txt', status = 'old')
+    do i = 1, INPUT_SIZE
+        read(1, *) tmp
+        fish(tmp) = 1 + fish(tmp)
+    end do
+    close(1)
 
-   n_fish = INPUT_SIZE
+    bigloop: do rounds = 1, 256
+        to_add = fish(0)
+        do i = 1, MAX
+            fish(i-1) = fish(i)
+        end do
+        fish(8) = to_add
+        fish(6) = to_add + fish(6)
+    end do bigloop
 
-   bigloop: do rounds = 1, 80
-      to_add = 0
-      do i = 1, n_fish
-        fish(i) = fish(i) - 1
-        if (fish(i) < 0) then
-            fish(i) = 6
-            to_add = 1 + to_add
-        end if
-      end do
-      do i = 1, to_add
-        n_fish = n_fish + 1
-        fish(n_fish) = 8
-      end do
-   end do bigloop
+    total = 0
 
-   print*, n_fish
+    do i = 0, MAX
+        total = fish(i) + total
+    end do
+
+    print*, total
 end program Lanternfish
--- README.md
+++ README.md
@@ -16,6 +16,7 @@ You can only use any programming language once.
   might really be worth exploring. I'm sure I've done everything incorrect in an ideomatic sense.
 * [Bingo](04/bingo.rkt) and [Reverse Bingo](04/reverse-bingo.rkt) (Racket). I was tired after a day of yard work, so I went with something that's at least a little familiar. (Though it's been some time since I've done anything with scheme, thus the ugly.)  
 * [Hydrothermal](05/hydrothermal.pp) (Pascal), with the input cleaned up to a more pascal-friendly four numbers instead of be-comma'd pairs with an arrow in between.
+* [Lanternfish](06/lanternfish.f90) (FORTRAN). `make ; ./lanternfish` to run. Need to have `gfortran` installed.
 
 ## PL's I'm comfortable enough working with on day 1
 
@@ -52,3 +53,4 @@ You can only use any programming language once.
 ## PL's entirely new to me
 
 * ~~Nim~~
+* ~~FORTRAN~~
